c1fda1777aee043205dde77dc2bcf605cbaf6142
d-anasx <djamaouianas@gmail.com>
2025-04-23 12:26:33 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/contact-page/',
+  base: '/contact-page/SmartListsPage',
   plugins: [react(),
     tailwindcss(),
   ],
